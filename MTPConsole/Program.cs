d43d3a8e0cdbf8189f674a3ff723c305d09274c1
dreamsyntax <dreamsyntax@gmail.com>
2023-08-06 19:51:54 -0700

    add auto shell extension for extracting to same dir

--- MTPConsole/Program.cs
+++ MTPConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using Microsoft.Win32;
+using System.Reflection;
 using MTPLib;
 
 namespace MTPConsole
@@ -10,18 +12,28 @@ namespace MTPConsole
 
         static void Main(string[] args)
         {
-            Console.WriteLine("MTPLib: Sample Application");
             Console.WriteLine("Usage: MTPConsole <paths to files to extract or directories to pack>");
             Console.WriteLine("Example: MTPConsole BkChaos.mtp BkLarva.mtp");
+            Console.WriteLine("\nRunning with no arguments registers the .MTP extension to allow double-clicking .MTP files for extraction");
 
-            foreach (var arg in args)
+            if (args.Length > 0)
             {
-                var fullPath = Path.GetFullPath(arg);
+                foreach (var arg in args)
+                {
+                    var fullPath = Path.GetFullPath(arg);
 
-                if (IsMtp(fullPath))
-                    ExtractMtp(fullPath);
-                else if (IsDirectory(fullPath))
-                    BuildMtp(fullPath);
+                    if (IsMtp(fullPath))
+                        ExtractMtp(fullPath);
+                    else if (IsDirectory(fullPath))
+                        BuildMtp(fullPath);
+                }
+            } else {
+                var classesKey = Registry.CurrentUser.OpenSubKey("Software", true)?.OpenSubKey("Classes", true);
+                var oneKey = classesKey.CreateSubKey(".mtp");
+                string myExecutable = Assembly.GetEntryAssembly().Location.Replace(".dll", ".exe");
+                string command = $"\"{myExecutable}\" \"%1\"";
+                var commandKey = oneKey.CreateSubKey("shell\\Open\\command");
+                commandKey.SetValue("", command);
             }
         }
 
